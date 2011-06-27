1dfabab3ad1c7b33580ae9dcaa915ee7bdc18540
Miguel Pastor <miguelinlas3@gmail.com>
2011-06-27 23:38:07 +0200

    Simple transaction management with Scala to compare with its AspecJ counterpart

---
+++ ScalaCacheExample/src/main/scala/com/blogspot/miguelinlas3/scala/transaction/manager/MockTransactionManager.scala
@@ -0,0 +1,20 @@
+package com.blogspot.miguelinlas3.scala.transaction.manager
+
+class MockTransactionManager {
+
+	def openTransaction(): Unit = {
+		println ("Opening transaction")
+	}
+	
+	def closeTransaction(): Unit = {
+		println("Closing transaction");
+	}
+	
+	def rollback(): Unit = {
+		println("Roll back previous work");
+	}
+	
+	def commit(): Unit = {
+		println("Commiting the work");
+	}
+}
